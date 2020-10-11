8e2bfb1aeb5c7dfed4de3e4296ae466061feb4d8
Long Phi <gameofnoobs17@gmail.com>
2020-10-11 22:49:15 +0700

    Continue with the DAL: Insert, Delete, Update methods are available on all classes, as well as a few other functions

--- CoffeeShopManager/DAL/DAL_Employee.cs
+++ CoffeeShopManager/DAL/DAL_Employee.cs
@@ -15,94 +15,143 @@ namespace DAL
     {
         public DataTable GetAllEmployees()
         {
-            DataTable dt = new DataTable();
+            DataTable dtEmp = new DataTable();
             DAL_Workers dalWr = new DAL_Workers();
             string qry = "SELECT * FROM [EMPLOYEES]";
+            SqlDataAdapter ada = new SqlDataAdapter(qry, this.conn);
+            
+            OpenConnection();
+            ada.Fill(dtEmp);
+            CloseConnection();
 
-            return dt;
+            var dtWorkers = dalWr.GetAllEmployeeWorkers();
+            var dtMerged = dtWorkers.Clone();
+            for (int i = 1; i < dtEmp.Columns.Count; ++i)
+            {
+                dtMerged.Columns.Add(dtEmp.Columns[i]);
+            }
+
+            var workerTab = dtWorkers.AsEnumerable();
+            var empTab = dtEmp.AsEnumerable();
+            var mergedRows = workerTab.Zip(empTab, (r1, r2) => r1.ItemArray.Concat(r2.ItemArray).ToArray());
+            foreach (object[] row in mergedRows)
+            {
+                dtMerged.Rows.Add(row);
+            }
+
+            return dtMerged;
         }
 
-        public DTO_Employee GetById(int id)
+        /// <summary>Gets the employee with the specified <c>Id</c> and only the <c>Id</c> of the <c>Manager</c> property; returns <returns>null</returns> if no employee with such <c>Id</c> exists</summary>
+        public DTO_Employee GetEmployeeInfoAndManagerId(int id) // Only gets the manager's id, which is already included in [EMPLOYEES] table
         {
+            
             DTO_Employee emp = null;
-            //
+            DAL_Workers dalWorkers = new DAL_Workers();
+            string qry = "SELECT * FROM [EMPLOYEES] WHERE Id = @id";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            OpenConnection();
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                emp = new DTO_Employee(dalWorkers.GetInfoById(reader.GetInt32(reader.GetOrdinal("Id"))));
+                emp.Address = reader.GetString(reader.GetOrdinal("Address"));
+
+                var dateofjoin = reader.GetDateTime(reader.GetOrdinal("DateOfJoin"));
+                emp.DateOfJoin = new DateTime(dateofjoin.Year, dateofjoin.Month, dateofjoin.Day);
+                emp.Salary = reader.GetDouble(reader.GetOrdinal("Salary"));
+
+                var empAccount = dalWorkers.GetUserInfoById(emp.Id);
+                emp.Account.ID = empAccount.ID;
+                emp.Manager.Id = reader.GetInt32(reader.GetOrdinal("ManagerId"));
+            }
+            CloseConnection();
 
             return emp;
         }
 
-        public bool Insert(DTO_Employee emp)
+        /// <summary>
+        /// Gets list of employees managed by the manager with the specified <c>Id</c>
+        /// </summary>
+        /// <param name="managerId">The manager's Id</param>
+        /// <returns></returns>
+        public List<DTO_Employee> GetEmployeesThroughManagerId(int managerId)
         {
-            try
+            List<DTO_Employee> empList = null;
+            string qry = "SELECT * FROM [EMPLOYEES] WHERE ManagerId = @manId";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@manId", managerId);
+
+            OpenConnection();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
             {
-                var dtinfo = new DataTable();
-                string qry = "SELECT * FROM [EMPLOYEES]";
-                var adapter = new SqlDataAdapter(qry, this.conn);
-                DataRow row;
-
-                OpenConnection();
-                adapter.Fill(dtinfo);
-                new SqlCommandBuilder(adapter);
-                row = dtinfo.NewRow();
-                //
-
-                CloseConnection();
-
-                return true;
+                var emp = GetEmployeeInfoAndManagerId(reader.GetInt32(reader.GetOrdinal("Id")));
+                empList.Add(emp);
             }
-            catch
-            {
-                return false;
-            }
+            CloseConnection();
+
+            return empList;
         }
 
-        public bool Delete(int id)
+        public DTO_Manager GetManagerInfo(DTO_Employee emp)
         {
-            try
-            {
-                var dtinfo = new DataTable();
-                string qry = "SELECT * FROM [EMPLOYEES]";
-                var adapter = new SqlDataAdapter(qry, this.conn);
-                DataRow row;
-
-                OpenConnection();
-                adapter.Fill(dtinfo);
-                new SqlCommandBuilder(adapter);
-                for (int i = 0; i < dtinfo.Rows.Count; ++i)
-                {
-                    if ((int)(dtinfo.Rows[i]["Id"]) == id)
-                    {
-                        dtinfo.Rows[i].Delete();
-                    }
-                }
-                adapter.Update(dtinfo);
-                CloseConnection();
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            DAL_Manager dalMan = new DAL_Manager();
+            return dalMan.GetById(emp.Manager.Id);
         }
 
-        public bool Update(DTO_Employee empUpdated)
+        public void Insert(DTO_Employee emp)
         {
-            var dtinfo = this.GetAllEmployees();
-            try
-            {
-                var row = dtinfo.Rows.Find(empUpdated);
+            DAL_Workers dalWorkers = new DAL_Workers();
+            string qry = "INSERT INTO [EMPLOYEES] VALUES (@workerId, @address, @dateofjoin, @salary, @managerId)";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            int workerId = dalWorkers.Insert(emp);
+            cmd.Parameters.AddWithValue("@workerId", workerId);
+            cmd.Parameters.AddWithValue("@address", emp.Address);
+            cmd.Parameters.AddWithValue("@dateofjoin", emp.DateOfJoin);
+            cmd.Parameters.AddWithValue("@salary", emp.Salary);
+            cmd.Parameters.AddWithValue("@managerId", emp.Manager.Id);
 
-                OpenConnection();
-                //
+            OpenConnection();
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+        }
 
-                CloseConnection();
+        public void Delete(DTO_Employee emp)
+        {
+            DAL_Workers dalWorkers = new DAL_Workers();
+            string qry = "DELETE FROM [EMPLOYEES] WHERE Id = @id";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@id", emp.Id);
 
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            dalWorkers.Delete(emp);
+            OpenConnection();
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+        }
+
+        /// <summary>
+        /// Updates the database where the employee's data has changed, using <c>Id</c> the as identifier
+        /// </summary>
+        /// <param name="empUpdated">The updated employee</param>
+        public void Update(DTO_Employee empUpdated)
+        {
+            DAL_Workers dalWorkers = new DAL_Workers();
+            string qry = "UPDATE [EMPLOYEE] " +
+                "SET Address = @address, DateOfJoin = @doj, " +
+                "Salary = @salary, ManagerId = @manId";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@address", empUpdated.Address);
+            cmd.Parameters.AddWithValue("@doj", empUpdated.DateOfJoin);
+            cmd.Parameters.AddWithValue("@salary", empUpdated.Salary);
+            cmd.Parameters.AddWithValue("@manId", empUpdated.Manager.Id);
+
+            dalWorkers.Update(empUpdated);
+            OpenConnection();
+            cmd.ExecuteNonQuery();
+            CloseConnection();
         }
     }
 }
--- CoffeeShopManager/DAL/DAL_Workers.cs
+++ CoffeeShopManager/DAL/DAL_Workers.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using DTO;
@@ -18,7 +20,6 @@ namespace DAL
                 "EmailAddress, Birthdate " +
                 "FROM [WORKERS] WHERE Id = @Id";
             SqlCommand cmd = new SqlCommand(qry, this.conn);
-            string[] bdate;
             cmd.Parameters.AddWithValue("@Id", id);
 
             OpenConnection();
@@ -33,36 +34,139 @@ namespace DAL
                 dtoWorker.Phone = reader.GetString(reader.GetOrdinal("PhoneNumber"));
                 dtoWorker.Account.Email = reader.GetString(reader.GetOrdinal("EmailAddress"));
 
-                bdate = reader.GetString(reader.GetOrdinal("Birthdate")).Split('/');
-                dtoWorker.Birthdate = new DateTime(Int32.Parse(bdate[0]), Int32.Parse(bdate[1]), Int32.Parse(bdate[2]));
+                var bdate = reader.GetDateTime(reader.GetOrdinal("Birthdate"));
+                dtoWorker.Birthdate = new DateTime(bdate.Year, bdate.Month, bdate.Day);
             }
             CloseConnection();
 
             return dtoWorker;
         }
 
-        public DTO_User GetUserInfoById(int id)
+        private DTO_Worker GetInfoByEmail(string email)
         {
-            DTO_User dtoUser = null;
-            string qry = "SELECT [USERS].Id FROM [USERS] " +
-                "INNER JOIN [WORKERS]" +
-                "ON [WORKERS].EmailAddress = [USERS].EmailAddress" +
-                "WHERE [WORKERS].Id = @Id";
+            DTO_Worker dtoWorker = null;
+            string qry = "SELECT * FROM [WORKERS] WHERE EmailAddress = @email";
             SqlCommand cmd = new SqlCommand(qry, this.conn);
-            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@email", email);
 
             OpenConnection();
-            SqlDataReader reader = cmd.ExecuteReader();
+            var reader = cmd.ExecuteReader();
             if (reader.Read())
             {
-                DAL_UserInfo uInfo = new DAL_UserInfo();
-                dtoUser = uInfo.GetById(reader.GetInt32(reader.GetOrdinal("Id")));
+                dtoWorker = GetInfoById(reader.GetInt32(reader.GetOrdinal("Id")));
             }
             CloseConnection();
 
-            return dtoUser;
+            return dtoWorker;
         }
 
-        // Thêm, xóa, sửa...
+        public DTO_User GetUserInfoByEmail(string email)
+        {
+            DAL_UserInfo dalUserInfo = new DAL_UserInfo();
+            return dalUserInfo.GetByEmail(email);
+        }
+
+        public DTO_User GetUserInfoById(int workerId)
+        {
+            return GetUserInfoByEmail(GetInfoById(workerId).Account.Email);
+        }
+
+        public DataTable GetAllManagerWorkers()
+        {
+            return GetAllWorkerOfPosition("Manager");
+        }
+
+        public DataTable GetAllEmployeeWorkers()
+        {
+            return GetAllWorkerOfPosition("Employee");
+        }
+
+        private DataTable GetAllWorkerOfPosition(string position)
+        {
+            DataTable dtPos = new DataTable();
+            string qry = "SELECT * FROM [WORKERS] WHERE Position = @position";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@position", position);
+            SqlDataAdapter ada = new SqlDataAdapter(cmd);
+
+            OpenConnection();
+            ada.Fill(dtPos);
+            CloseConnection();
+
+            return dtPos;
+        }
+
+        /// <summary>
+        /// Inserts worker and returns their <c>Id</c>, which mainly serves as a foreign key for tables [EMPLOYEES] and [MANAGERS]
+        /// </summary>
+        /// <param name="dtoWorker"></param>
+        /// <returns>The <c>Id</c> of the worker inserted</returns>
+        public int Insert(DTO_Worker dtoWorker)
+        {
+            DAL_UserInfo dalUserInfo = new DAL_UserInfo();
+            string qry = "INSERT INTO [WORKERS] VALUES " +
+                "(@firstName, @lastName, @gender, " +
+                "@position, @phoneNumber, @email, " +
+                "@birthdate, @accountId)";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@firstName", dtoWorker.Firstname);
+            cmd.Parameters.AddWithValue("@lastName", dtoWorker.Lastname);
+            cmd.Parameters.AddWithValue("@gender", dtoWorker.Gender);
+            cmd.Parameters.AddWithValue("@position", dtoWorker.Position);
+            cmd.Parameters.AddWithValue("@phoneNumber", dtoWorker.Phone);
+            cmd.Parameters.AddWithValue("@email", dtoWorker.Account.Email);
+            cmd.Parameters.AddWithValue("@birthdate", dtoWorker.Birthdate);
+
+            dalUserInfo.Insert(dtoWorker.Account);
+            int accountId = dalUserInfo.GetByEmail(dtoWorker.Account.Email).ID;
+            cmd.Parameters.AddWithValue("@accountId", accountId);
+
+            OpenConnection();
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+
+            return GetInfoByEmail(dtoWorker.Account.Email).Id;
+        }
+
+        public void Delete(DTO_Worker dtoWorker)
+        {
+            DAL_UserInfo dalUserInfo = new DAL_UserInfo();
+            string qry = "DELETE FROM [WORKERS] WHERE Id = @id";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@id", dtoWorker.Id);
+
+            dalUserInfo.Delete(dtoWorker.Account);
+            OpenConnection();
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+        }
+
+        /// <summary>
+        /// Updates the database where the worker's data has changed, using <c>Id</c> the as identifier
+        /// </summary>
+        /// <param name="dtoWorkerUpdated">The updated worker</param>
+        public void Update(DTO_Worker dtoWorkerUpdated)
+        {
+            DAL_UserInfo dalUserInfo = new DAL_UserInfo();
+            string qry = "UPDATE [WORKERS] " +
+                "SET FirstName = @fname, LastName = @lname, Gender = @gender, " +
+                "Position = @position, PhoneNumber = @phone, EmailAddress = @email, " +
+                "Birthdate = @bdate " +
+                "WHERE Id = @id";
+            SqlCommand cmd = new SqlCommand(qry, this.conn);
+            cmd.Parameters.AddWithValue("@fname", dtoWorkerUpdated.Firstname);
+            cmd.Parameters.AddWithValue("@lname", dtoWorkerUpdated.Lastname);
+            cmd.Parameters.AddWithValue("@gender", dtoWorkerUpdated.Gender);
+            cmd.Parameters.AddWithValue("@position", dtoWorkerUpdated.Position);
+            cmd.Parameters.AddWithValue("@phone", dtoWorkerUpdated.Phone);
+            cmd.Parameters.AddWithValue("@email", dtoWorkerUpdated.Account.Email);
+            cmd.Parameters.AddWithValue("@bdate", dtoWorkerUpdated.Birthdate);
+            cmd.Parameters.AddWithValue("@id", dtoWorkerUpdated.Id);
+
+            dalUserInfo.Update(dtoWorkerUpdated.Account);
+            OpenConnection();
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+        }
     }
 }
