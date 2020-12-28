79b7763ffb917522bb07ea70e70769a3c7f07097
feelings93 <19521298@gm.uit.edu.vn>
2020-12-28 13:31:15 +0700

    Update dbscrisp

--- CoffeeShopManager/GUI/Properties/Settings.Designer.cs
+++ CoffeeShopManager/GUI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace GUI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -38,7 +38,7 @@ namespace GUI.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=10.0.131.2\\SQLEXPRESS,1433;Database=NewDB;User ID=sa;Password=123456;Multi" +
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=10.0.131.2\\SQLEXPRESS,1433;Database=ONEDB;User ID=sa;Password=123456;Multi" +
             "pleActiveResultSets=True;")]
         public string ConnectionString {
             get {
