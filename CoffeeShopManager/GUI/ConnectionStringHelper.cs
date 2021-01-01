95dd9daae675ccd570661f10fe44cd48c1962378
Long Phi <gameofnoobs17@gmail.com>
2021-01-01 14:43:01 +0700

    Connection string and small adjustments

--- CoffeeShopManager/GUI/ConnectionStringHelper.cs
+++ CoffeeShopManager/GUI/ConnectionStringHelper.cs
@@ -11,9 +11,7 @@ namespace GUI
     {
         public static string GetConnectionString()
         {
-            //return ConfigurationManager.ConnectionStrings["GUI.Properties.Settings.ConnectionString"].ConnectionString;
-            //return "Server = localhost; Database = NewDB; User ID = sa; Password = onetwothree; MultipleActiveResultSets = True;";
-            return "Server = 192.168.43.195, 1433; Database = NewDB; User ID = sa; Password = 123456; MultipleActiveResultSets = True;";
+            return ConfigurationManager.ConnectionStrings["GUI.Properties.Settings.ConnectionString"].ConnectionString;
         }
     }
 }
